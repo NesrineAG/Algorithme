24d671c9ae2602089675fee83065a5558a8df6f4
HP <HP@DESKTOP-F0IFFGU>
2024-04-27 00:01:20 -0700

    Ajoutez des fichiers projet.

---
+++ ArrayInsertion/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayInsertion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            array = InsertValue(array, 10, 2);
+
+            Console.WriteLine("Array after insertion:");
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+        }
+
+        static int[] InsertValue(int[] arr, int value, int index)
+        {
+            int[] newArray = new int[arr.Length + 1];
+
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = arr[i];
+            }
+
+            newArray[index] = value;
+
+            for (int i = index; i < arr.Length; i++)
+            {
+                newArray[i + 1] = arr[i];
+            }
+
+            return newArray;
+        }
+
+
+    }
+}
